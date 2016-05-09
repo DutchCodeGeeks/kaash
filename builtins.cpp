bda1d94bf46dd2e4ea8b9ebe285464e0af11f161
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2016-05-09 20:30:26 +0200

    remove unnecessary builtins

--- builtins.cpp
+++ builtins.cpp
@@ -16,9 +16,6 @@ unordered_map<string, function<string(vector<string>)>> builtins = {
 		chdir(loc.c_str());
 		return string("");
 	}},
-	{ string("pwd"), [] (vector<string> args) {
-		return varstore.get("PWD");
-	}},
 	{ string("exit"), [] (vector<string> args) {
 		int exitCode = 0;
 		if ((int)args.size() > 0) {
@@ -28,36 +25,14 @@ unordered_map<string, function<string(vector<string>)>> builtins = {
 		exit(exitCode);
 		return "";
 	}},
-	{ string("echo"), [] (vector<string> args) {
-		if (args.size() == 0) {
-			return string("");
-		} else {
-			return joinVector(args, " ");
+	{ string("alias"), [] (vector<string> args) {
+		if (args.size() != 2) {
+			return string("expected 2 arguments");
 		}
-	}},
-	{ string("ls"), [] (vector<string> args) {
-		string loc;
-		stringstream ss;
+		string name = args[0];
+		string def = args[1];
 
-		if (args.size() == 0) {
-			loc = ".";
-		} else {
-			loc = args[0];
-		}
-
-		Maybe<vector<dirEntry>> mdir = readDir(loc);
-		if (mdir.isNothing()) {
-			return args[0];
-		}
-		const vector<dirEntry> &dir = mdir.fromJust();
-		for (unsigned int i = 0; i < dir.size(); i++) {
-			const dirEntry &entry = dir[i];
-
-			if (i != 0) ss << endl;
-			ss << entry.name;
-		}
-
-		return ss.str();
+		return string("");
 	}},
 };
 
